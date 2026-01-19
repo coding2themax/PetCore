7890525dad28e688db472e50bbf398b57ba8899c
max quinones <maximilian.quinones@gmail.com>
2026-01-19 16:08:46 -0700

    Replace PetIntakeController with PetIntakeRouter for a reactive routing approach

---
+++ backend-service/src/main/java/com/coding2themax/petcore/pet/service/profile/api/router/PetIntakeRouter.java
@@ -0,0 +1,21 @@
+package com.coding2themax.petcore.pet.service.profile.api.router;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.coding2themax.petcore.pet.service.profile.api.handler.PetIntakeHandler;
+
+@Configuration
+public class PetIntakeRouter {
+
+  public static final String PET_INTAKE_BASE_URL = "/api/v1/pets/intake";
+
+  public RouterFunction<ServerResponse> route(PetIntakeHandler handler) {
+    return RouterFunctions
+        .route(RequestPredicates.POST(PET_INTAKE_BASE_URL), handler::handleIntake);
+
+  }
+}
