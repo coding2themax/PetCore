09a253129acc33f0bcfdf5900e1cafa4458d5a76
max quinones <maximilian.quinones@gmail.com>
2026-01-19 11:22:55 -0700

    Add unit test for main method in PetServiceApplication

--- backend-service/src/test/java/com/coding2themax/petcore/pet/service/PetServiceApplicationTests.java
+++ backend-service/src/test/java/com/coding2themax/petcore/pet/service/PetServiceApplicationTests.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class PetServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainMethodStartsApplication() {
+		// Test that main method doesn't throw an exception
+		PetServiceApplication.main(new String[] {});
 	}
 
 }
